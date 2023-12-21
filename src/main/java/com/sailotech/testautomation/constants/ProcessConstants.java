3952d4a41d484d1a31b00e59aa7900545e6f31e9
lavanya.chinta <lavanya.chinta@sailotech.com>
2023-12-21 19:28:00 +0530

    first commit

---
+++ src/main/java/com/sailotech/testautomation/constants/ProcessConstants.java
@@ -0,0 +1,22 @@
+package com.sailotech.testautomation.constants;
+
+public class ProcessConstants {
+
+	public static final String IS_DISPAYED = "isDisplayed";
+	public static final String IS_ELEMENT_PRESENT = "isElementPresent";
+	public static final String IS_ELEMENT_NOT_PRESENT = "isElementNotPresent";
+	public static final String IS_SELECTED = "isSelected";
+	public static final String IS_NOT_SELECTED = "isNotSelected";
+	public static final String VRIFYSTRINGS = "verifyStrings";
+	public static final String VERIFYTEXT = "verifyText";
+	
+	public static final String TYPE = "typeText";
+	public static final String CLICKELEMENT = "clickElement";
+	public static final String DOUBLECLICKELEMENT = "doubleClickElement";
+	public static final String TYPETEXTWITHENTER = "typeTextWithEnter";
+	public static final String TYPETEXTWITHTABKEY = "typeTextWithTabKey";
+	public static final String TEXTCLEAR = "textClear";
+	public static final String GETTEXT = "getText";
+	public static final String EXTRACTDATAFROMPDF = "extractDataFromPDF";
+	public static final String CLICKLASTCHECKBOXINLIST = "clickLastCheckBoxInList";
+}
